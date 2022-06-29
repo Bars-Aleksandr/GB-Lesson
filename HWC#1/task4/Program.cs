eeb5b0fbeac4eb0bf2b372175767d0834a32333c
Aleksandr <bas8319@yahoo.com>
2022-06-29 15:03:15 +0300

    добавили условие задачи

--- HWC#1/task4/Program.cs
+++ HWC#1/task4/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
