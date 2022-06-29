4afdc6ad948557160d749731ac75bf9393beac6a
Aleksandr <bas8319@yahoo.com>
2022-06-29 17:53:22 +0300

    Lesson1 task6 successfully completed

--- HWC#1/task6/Program.cs
+++ HWC#1/task6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+Console.Write("Введите любой целое число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+//int b=a % 2;
+//Сonsole.WriteLine(b);
+if ((a % 2)==0)
+{
+   Console.Write("Введенное число четное");
+}
+else 
+{
+  Console.Write("Введенное число нечетное");
+}
