7209a6577552203265b8b807757f9316e3db0558
Aleksandr <bas8319@yahoo.com>
2022-06-29 18:23:24 +0300

    Lesson1 task8 successfully completed

--- HWC#1/task8/Program.cs
+++ HWC#1/task8/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+Console.WriteLine("Введите число N до которого необходимо вывести все четные числа");
+int n = int.Parse(Console.ReadLine());
+int count=1;
+Console.Write("Четные числа ");
+while (count<=n)
+{
+    if ((count % 2)==0)
+{
+  Console.Write($"{ count } ");
+}
+
+count++;
+
+}
