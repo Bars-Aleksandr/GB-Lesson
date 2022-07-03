efe112d907b1cb5fda470707af1184d976f9a6bb
Aleksandr <bas8319@yahoo.com>
2022-07-03 15:34:27 +0300

    Added a solution to the task2 from the seminar

--- SeminarCsharp2/Seminar2Task1/example2/Program.cs
+++ SeminarCsharp2/Seminar2Task1/example2/Program.cs
@@ -1,2 +1,17 @@
 ﻿// 2. Напишите программу, которая выводит случайное число
 // из отрезка [10, 99] и показывает наибольшую цифру числа. При помощи методов.
+int Maxnumber(int num)
+{
+
+if (num % 10 > num / 10)
+{
+return num % 10;
+}
+else
+return num / 10;
+}
+int SecondNum = new Random().Next(10, 100);
+Maxnumber(SecondNum);
+
+Console.WriteLine (SecondNum) ;
+Console.WriteLine (Maxnumber(SecondNum)) ;
