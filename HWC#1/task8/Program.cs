cb4bc9f1f1c78db4c2c67d3993c779fc8df710c4
Aleksandr <bas8319@yahoo.com>
2022-07-03 13:24:28 +0300

    Saving on the GitHub task8

--- HWC#1/task8/Program.cs
+++ HWC#1/task8/Program.cs
@@ -2,15 +2,18 @@
 // числа от 1 до N.
 Console.WriteLine("Введите число N до которого необходимо вывести все четные числа");
 int n = int.Parse(Console.ReadLine());
+
 int count=1;
+
 Console.Write("Четные числа ");
+
 while (count<=n)
 {
-    if ((count % 2)==0)
-{
-  Console.Write($"{ count } ");
-}
+  if ((count % 2) == 0)
+  {
+    Console.Write($"{ count } ");
+  }
 
-count++;
+  count++;
 
 }
