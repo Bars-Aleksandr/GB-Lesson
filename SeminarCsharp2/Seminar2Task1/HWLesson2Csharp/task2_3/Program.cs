b90bea47963990e84f88a77dc50bc1d9e996c853
Aleksandr <bas8319@yahoo.com>
2022-07-03 18:32:45 +0300

    task 3 for seminar 2 completed

---
+++ SeminarCsharp2/Seminar2Task1/HWLesson2Csharp/task2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры
+//нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//326
+void LastDigital(int Number)
+{
+    Console.WriteLine($"{Number} --> {Number % 10}");
+}
+Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int Number = int.Parse(Console.ReadLine());
+
+if (Number < 100)
+{
+    Console.WriteLine($"{Number} --> третьей цифры нет");
+}
+else
+{
+    LastDigital(Number);
+}
