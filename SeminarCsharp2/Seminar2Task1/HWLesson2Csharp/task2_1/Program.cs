12205f3998723cd466edf0f06bfb536ac5f26ab0
Aleksandr <bas8319@yahoo.com>
2022-07-03 17:28:09 +0300

    Task2 performed

---
+++ SeminarCsharp2/Seminar2Task1/HWLesson2Csharp/task2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+int AverageDigit(int num)
+{
+    return (num / 10) % 10;
+}
+Console.WriteLine("Введите трехзначное число и нажмите клавишу Enter: ");
+int Number = int.Parse(Console.ReadLine());
+
+if (Number > 99 & Number < 1000)
+{
+    Console.WriteLine(AverageDigit(Number));
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число ");
+}
