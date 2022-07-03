19d7d5ff2d4f48fbea5cc29c185aa26737fa0dc2
Aleksandr <bas8319@yahoo.com>
2022-07-03 18:12:40 +0300

    task 2 for seminar 2 completed

---
+++ SeminarCsharp2/Seminar2Task1/HWLesson2Csharp/task2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+void DelAverageDigital(int Number)
+{
+    Console.WriteLine($"{Number}-->{Number / 100}{Number % 10}");
+}
+int RND = new Random().Next(100, 1000);
+Console.WriteLine(RND);
+DelAverageDigital(RND);
