c6b0ae1bdaffc7f8ac6f0c568100d0814ac49d67
Aleksandr <bas8319@yahoo.com>
2022-07-03 14:45:26 +0300

    Task1 from Seminar2 Csharp successfully completed

--- SeminarCsharp2/Seminar2Task1/example1/Program.cs
+++ SeminarCsharp2/Seminar2Task1/example1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+int LastDigit(int num)
+{
+return num % 10;
+}
+Console.Write("Введите трехзначное число и нажмите клавишу Enter: ");
+int Number = int.Parse(Console.ReadLine());
+Console.WriteLine(LastDigit(Number));
