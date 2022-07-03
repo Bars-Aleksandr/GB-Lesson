61507cc559c74d27ac22bdb89f4477ccf626033d
Aleksandr <bas8319@yahoo.com>
2022-07-03 13:22:48 +0300

    Saving on the GitHub task6

--- HWC#1/task6/Program.cs
+++ HWC#1/task6/Program.cs
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
 // (делится ли оно на два без остатка).
 Console.Write("Введите любой целое число и нажмите клавишу Enter: ");
 int a = int.Parse(Console.ReadLine());
-//int b=a % 2;
-//Сonsole.WriteLine(b);
-if ((a % 2)==0)
+
+if ((a % 2) == 0)
 {
    Console.Write("Введенное число четное");
 }
@@ -13,7 +11,3 @@ else
 {
   Console.Write("Введенное число нечетное");
 }
-=======
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
->>>>>>> 100c91c (Add the task)
