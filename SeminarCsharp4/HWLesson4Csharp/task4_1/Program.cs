da14a489134a846ac9909e8696c209288eddd25d
Aleksandr <bas8319@yahoo.com>
2022-07-09 11:38:48 +0300

    Проект №1 домашнего задания для 4-ого семинара: саздали тело программы в котором производится возведение в степень, без проверок и функций

--- SeminarCsharp4/HWLesson4Csharp/task4_1/Program.cs
+++ SeminarCsharp4/HWLesson4Csharp/task4_1/Program.cs
@@ -2,8 +2,14 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-Console.Write("Введите число, которое необходимо возвести в степень");
+Console.Write("Введите число, которое необходимо возвести в степень ");
 double A = double.Parse(Console.ReadLine());
 
-Console.Write("Введите степень,в которуе необходимо возвести введенное число");
+Console.Write("Введите степень, в которую необходимо возвести введенное число ");
 int B = int.Parse(Console.ReadLine());
+double ACopy = A;
+for (int count = 1; count < B; count++)
+{
+    ACopy = ACopy * ACopy;
+}
+Console.WriteLine($"Число {A} при возведении в натуральную степень {B} равно {ACopy}");
