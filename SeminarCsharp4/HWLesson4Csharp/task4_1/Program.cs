40353dd6626f85a92762914d105f5ab332fa5e75
Aleksandr <bas8319@yahoo.com>
2022-07-09 13:55:41 +0300

    Проект №1 домашнего задания для 4-ого семинара: рабочий вариант программы V 1.0

--- SeminarCsharp4/HWLesson4Csharp/task4_1/Program.cs
+++ SeminarCsharp4/HWLesson4Csharp/task4_1/Program.cs
@@ -1,18 +1,16 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-void Exponent(double digital, int exp)
+double Exponent(double digital, int exp)
 {
     double result = 1;
-    //Console.WriteLine(digital);
+
     for (int count = 1; count <= exp; count++)
     {
         result = result * digital;
-        Console.WriteLine(count);
-        Console.WriteLine(result);
     }
-    //Console.WriteLine(digital);
-    //return digital;
+
+    return result;
 }
 
 Console.Write("Введите число, которое необходимо возвести в степень ");
@@ -21,11 +19,11 @@ double A = double.Parse(Console.ReadLine());
 Console.Write("Введите натуральную степень, в которую необходимо возвести введенное число ");
 int B = int.Parse(Console.ReadLine());
 Exponent(A, B);
-//if (B <= 0)
-//{
-//    Console.WriteLine(" Вы ввели не натуральную степень");
-//}
-//else
-//{
-//   Console.WriteLine($"Число {A} при возведении в натуральную степень {B} равно {Exponent(A, B)}");
-//}
+if (B <= 0)
+{
+    Console.WriteLine(" Вы ввели не натуральную степень");
+}
+else
+{
+    Console.WriteLine($"Число {A} при возведении в натуральную степень {B} равно {Exponent(A, B)}");
+}
