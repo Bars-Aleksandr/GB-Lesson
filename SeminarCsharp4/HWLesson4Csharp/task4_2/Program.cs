137ceab39a6d51920ae88dd1bc9cae6b584592f1
Aleksandr <bas8319@yahoo.com>
2022-07-10 20:22:17 +0300

    Проект №2 домашнего задания для 4-ого семинара: добавили метод определения количества цифр в числе

--- SeminarCsharp4/HWLesson4Csharp/task4_2/Program.cs
+++ SeminarCsharp4/HWLesson4Csharp/task4_2/Program.cs
@@ -2,5 +2,21 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+int numberDigit(string stringVar)
+{
+    string numDigStr = stringVar.ToString();
+    return numDigStr.Length;
+}
+
+//int SeparationDigit(int initVar)
+//{
+// A = A / 10;
+//}
 Console.Write("Введите любое положительное целое число: ");
-int gitit = int.Parse(Console.ReadLine());
+string stringDigit = Console.ReadLine();
+int size = numberDigit(stringDigit);
+
+//string numDigStr = stringVar.ToString();
+//int size = numDigStr.Length;
+
+Console.WriteLine(size);
