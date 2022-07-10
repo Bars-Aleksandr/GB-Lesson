c49d5d48257a64529da9b7ad78584084c55cffce
Aleksandr <bas8319@yahoo.com>
2022-07-10 22:01:44 +0300

    Проект №3 домашнего задания для 4-ого семинара: создали метод заполняющий массив случайными числами

--- SeminarCsharp4/HWLesson4Csharp/task4_3/Program.cs
+++ SeminarCsharp4/HWLesson4Csharp/task4_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+//Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+//метод создающий массив заданой длинны случайными числами
+int[] arrayRND(int length)
+{
+    int[] arrRND = new int[length];
+    for (int i = 0; i <= length; i++)
+    {
+        arrRND[i] = new Random().Next();
+    }
+    return arrRND;
+}
+
+int lengthArray = 7;
+//arrayRND(lengthArray);
