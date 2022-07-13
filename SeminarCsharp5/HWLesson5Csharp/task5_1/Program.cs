fb9e62d71e43fb92e13c63feadc9fceca9c6c99f
Aleksandr <bas8319@yahoo.com>
2022-07-13 12:12:26 +0300

    Задача №1 домашнего задания для 5-ого семинара: задали требуемый массив и организовали его вывод

--- SeminarCsharp5/HWLesson5Csharp/task5_1/Program.cs
+++ SeminarCsharp5/HWLesson5Csharp/task5_1/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 // ПРИМЕР : [345, 897, 568, 234] -> 2
+
+
+//метод создающий массив заданой длинны случайными числами
+//int[] inputArrayRND(int length)
+//{
+//   int[] arrRND = new int[length];
+//  for (int i = 0; i < length; i++)
+// {
+//     arrRND[i] = new Random().Next(0, 100);
+// }
+
+// return arrRND;
+//}
+
+//метод для вывода массива на экран
+void printArray(int[] arrayIn)
+{
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        Console.Write($"{arrayIn[i]} ");
+    }
+}
+
+// тело основной программы
+//Console.Write("Введите длину создаваемого массива ");
+//int lengthArray = int.Parse(Console.ReadLine());
+
+int[] inArray = { 345, 897, 568, 234 };
+printArray(inArray);
