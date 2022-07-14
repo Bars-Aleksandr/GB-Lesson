daff5b6a0efbd4961111e4154350977c6248166b
Aleksandr <bas8319@yahoo.com>
2022-07-14 07:45:22 +0300

    Задача №3 домашнего задания для 5-ого семинара: первый вариант решения задачи

--- SeminarCsharp5/HWLesson5Csharp/task5_3/Program.cs
+++ SeminarCsharp5/HWLesson5Csharp/task5_3/Program.cs
@@ -1,3 +1,51 @@
 ﻿// Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементов массива.
 // ПРИМЕР: [3, 7, 22, 2, 78] -> 76
+
+//метод находнения min-элемент одномерного массива.
+double[] SearchMinMax(double[] baseArray)
+{
+    double[] MinMaxArr = new double[2]; //создали массив в котором первым элементом будет min значение, а вторым max значение
+    MinMaxArr[0] = baseArray[0];
+    MinMaxArr[1] = baseArray[1];
+    for (int i = 2; i < baseArray.Length; i++)
+    {
+        if (baseArray[i] < MinMaxArr[0])
+        {
+            MinMaxArr[0] = baseArray[i];
+        }
+        if (baseArray[i] > MinMaxArr[1])
+        {
+            MinMaxArr[1] = baseArray[i];
+        }
+    }
+
+    return MinMaxArr;
+}
+
+//метод для вывода массива на экран
+void printArray(double[] arrayIn)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        Console.Write($"{arrayIn[i]} ");
+    }
+    Console.Write("]");
+}
+
+// тело основной программы
+
+double[] sourceArray = { 4, 6, 4, 67 };
+
+printArray(sourceArray);
+
+printArray(SearchMinMax(sourceArray));
+Console.WriteLine();
+
+double[] MinMax = new double[2];
+
+MinMax = SearchMinMax(sourceArray);
+
+//int result = SearchMinMax[1] - SearchMinMax[0];
+Console.WriteLine($" --> {MinMax[1] - MinMax[0]}.");
