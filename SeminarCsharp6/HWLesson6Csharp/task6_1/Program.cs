e8a3d0e7d9cea33cb0398c3e58af5711cef26bff
Aleksandr <bas8319@yahoo.com>
2022-07-23 07:43:42 +0300

    Задача №1 домашнего задания для 6-ого семинара, вариант при котором разбирался как работать с String как с массивом

---
+++ SeminarCsharp6/HWLesson6Csharp/task6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите последовательность чисел");
+string num = Console.ReadLine();
+int length = num.Length;
+int countMinus = 0;
+int countSpace = 0;
+for (int i = 0; i < length; i++)
+{
+    if (num[i] == '-')  
+        countMinus +=1;
+
+    if (num[i] == ' ')  
+        countSpace +=1;
+    
+}
+    Console.WriteLine($"Число отрицательных элементов равно {countMinus}");
+    Console.WriteLine($"Число пробелов равно {countSpace}");
+    Console.WriteLine($"Число положительных элементов равно {(countSpace+1)-countMinus}");
