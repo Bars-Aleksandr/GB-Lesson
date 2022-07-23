6fd98d08d59d1bae0a69aa109517a6e8e5018e44
Aleksandr <bas8319@yahoo.com>
2022-07-23 09:31:08 +0300

    Задача №2 домашнего задания для 6-ого семинарa - готова

---
+++ SeminarCsharp6/HWLesson6Csharp/task6_2/Program.cs
@@ -0,0 +1,31 @@
+﻿ //Напишите программу, которая найдёт точку пересечения двух прямых,
+ // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double ResultX(double copyB1, double copyK1, double copyB2, double copyK2)
+{
+   return (copyB2 - copyB1) / (copyK1 - copyK2);
+}
+
+double ResultY(double X, double copyK1, double copyB1)
+{
+   return copyK1*X + copyB1;
+}
+
+Console.Write("Введите значение b1= ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k1= ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b2= ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k2= ");
+double k2 = double.Parse(Console.ReadLine());
+    
+
+double x = ResultX(b1, k1, b2, k2);
+double y = ResultY(x, k1, b1);
+
+Console.WriteLine($"Координаты точки пересечения двух прямых ({x,2} , {y,2})");
