c4c20702a53ca915b7fbcb6152876aa55a8939b9
Aleksandr <bas8319@yahoo.com>
2022-07-23 11:43:15 +0300

    добавил создание и вывод двумерногомассива

---
+++ SeminarCsharp6/HWLesson6Csharp/task6_3/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача: Напишите программу, которая будет создавать копию заданного двумерного массива с помощью поэлементного копирования.
+
+
+//создание случайного двумерного массива
+int[,] arrayRND(int n, int m)
+{
+    int[,] arrRND = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arrRND[i,j] = new Random().Next(0, 100);    
+        }
+    }
+
+    return arrRND;
+}
+
+//вывод двумерного массива на экран
+void printArray(int[,] arrayIn, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write($"{arrayIn[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+Console.Write($"Введите число строк n= ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write($"Введите число строк m= ");
+int m = int.Parse(Console.ReadLine());
+
+printArray(arrayRND(2, 5), 2, 5);
