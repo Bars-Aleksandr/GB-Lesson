827364a206a090241f1a8ecafc4f679dad7e8026
Aleksandr <bas8319@yahoo.com>
2022-07-30 11:11:01 +0300

    решение задачи 3 к семинару6-му готово

--- SeminarCsharp6/HWLesson6Csharp/task6_3/Program.cs
+++ SeminarCsharp6/HWLesson6Csharp/task6_3/Program.cs
@@ -2,7 +2,7 @@
 
 
 //создание случайного двумерного массива
-int[,] arrayRND(int n, int m)
+int[,] ArrayRND(int n, int m)
 {
     int[,] arrRND = new int[n, m];
     for (int i = 0; i < n; i++)
@@ -17,7 +17,7 @@ int[,] arrayRND(int n, int m)
 }
 
 //вывод двумерного массива на экран
-void printArray(int[,] arrayIn, int n, int m)
+void PrintArray(int[,] arrayIn, int n, int m)
 {
     for (int i = 0; i < n; i++)
     {
@@ -29,10 +29,39 @@ void printArray(int[,] arrayIn, int n, int m)
         Console.WriteLine();
     }
 }
+
+
+// копирование двумерного массива
+int[,] CopyingArray(int[,] inputArray)
+{
+    int[,] arr = new int[inputArray.GetLength(0), inputArray.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = inputArray[i,j];    
+        }
+    }
+
+    return arr;
+}
+
+
 Console.Write($"Введите число строк n= ");
 int n = int.Parse(Console.ReadLine());
 
-Console.Write($"Введите число строк m= ");
+Console.Write($"Введите число столбцов m= ");
 int m = int.Parse(Console.ReadLine());
 
-printArray(arrayRND(2, 5), 2, 5);
+
+int[,] baseArray = new int[n,m];
+int[,] copyArray = new int[n,m];
+
+baseArray = ArrayRND(n,m);
+Console.WriteLine("Исходный массив заполненый случайными целыми числами");
+PrintArray(baseArray,n,m);
+Console.WriteLine();
+
+copyArray = CopyingArray(baseArray);
+Console.WriteLine("Скопированный массив");
+PrintArray(copyArray,n,m);
