81a19c9abb8d2fdfe29ac537aaaf294507d0018a
Aleksandr <bas8319@yahoo.com>
2022-07-30 14:17:57 +0300

    Task№1 for seminar7 - successfully completed

--- SeminarCsharp7/HWLesson7Csharp/task7_1/Program.cs
+++ SeminarCsharp7/HWLesson7Csharp/task7_1/Program.cs
@@ -3,3 +3,46 @@
 //0,5 7 -2 -0,2
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
+
+//создание случайного двумерного массива
+double[,] ArrayRND(int n, int m)
+{
+    double[,] arrRND = new double[n, m];
+    Random nRND = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arrRND[i,j] = Math.Round(nRND.NextDouble()*(10+10)-10,1);    
+        }
+    }
+
+    return arrRND;
+}
+
+//вывод двумерного массива на экран
+void PrintArray(double[,] arrayIn, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write($"{arrayIn[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write($"Введите число строк n= ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write($"Введите число столбцов m= ");
+int m = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Полученый массив заполненый случайными числами");
+PrintArray(ArrayRND(n,m),n,m);
+Console.WriteLine();
+
