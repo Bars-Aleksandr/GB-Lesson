7d95f45baed336d97bfd9a3f2f6ffacedb7e9e95
Aleksandr <bas8319@yahoo.com>
2022-07-30 19:01:01 +0300

    Task№2 for seminar7 - successfully completed

--- SeminarCsharp7/HWLesson7Csharp/task7_2/Program.cs
+++ SeminarCsharp7/HWLesson7Csharp/task7_2/Program.cs
@@ -10,3 +10,62 @@
 
 //1 7 -> такого числа в массиве нет
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //создание случайного двумерного массива
+        int[,] ArrayRND()
+        {
+            int n = new Random().Next(1, 10);
+            int m = new Random().Next(1, 10);
+            //int n = 3;
+            //int m = 5;
+            int[,] arrRND = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arrRND[i, j] = new Random().Next(-10, 10);
+                }
+            }
+
+            return arrRND;
+        }
+
+        //вывод двумерного массива на экран
+        void PrintArray(int[,] arrayIn)
+        {
+            for (int i = 0; i < arrayIn.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayIn.GetLength(1); j++)
+                {
+                    Console.Write($"{arrayIn[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        int[,] arr = ArrayRND();
+        PrintArray(arr);
+
+        Console.Write($"Введите позицию числа по вертикали i= ");
+        int i1 = int.Parse(Console.ReadLine());
+
+        Console.Write($"Введите позицию числа по горизонтали j=");
+        int j1 = int.Parse(Console.ReadLine());
+       
+        if (i1 <= arr.GetLength(0) & j1 <= arr.GetLength(1) &
+                i1 > 0 & j1 > 0
+            )
+        {
+            Console.WriteLine($"Данной позиции соответсвует число {arr[i1 - 1, j1 - 1]} ");
+        }
+        else
+        {
+            Console.WriteLine("Числа с данной позицией нет");
+        }
+    }
+}
