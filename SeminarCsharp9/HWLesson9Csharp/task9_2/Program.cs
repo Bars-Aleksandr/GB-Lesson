196dd654247ac963912cb97798b6b7e10af379ec
Aleksandr <bas8319@yahoo.com>
2022-08-07 22:14:02 +0300

    Solution task №1 for seminar №9 successfully completed (This solved using recursion)

---
+++ SeminarCsharp9/HWLesson9Csharp/task9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+            натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int SumNumbers(int m, int n)
+{
+if (n < m) return 0;
+return SumNumbers(m, n-1) + n;
+
+}
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(M, N));
